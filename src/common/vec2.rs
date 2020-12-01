b70c672169e524384163fff401f4e3c8352aac4f
jacobguenther <jacobrwguenther@protonmail.com>
2020-12-01 00:58:28 -0900

    Add order trait to Vec2.

--- src/common/vec2.rs
+++ src/common/vec2.rs
@@ -23,7 +23,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub struct Vec2<T> {
 	pub x: T,
 	pub y: T,
